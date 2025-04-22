4230837ecfff53b974f9cccd1102ea4c980c521d
Makis <karacostas.jim@gmail.com>
2025-04-22 18:08:10 +0300

    Updates

---
+++ precache-manifest.0b994ff9a7457a222bf924376ca026aa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eb8a9b574929e8cbffa78bfdf8095f0",
+    "url": "/NutriTrack/index.html"
+  },
+  {
+    "revision": "66f35dd8acfe69790839",
+    "url": "/NutriTrack/static/js/2.bdc847f0.chunk.js"
+  },
+  {
+    "revision": "fca338dfcc8305a913fe",
+    "url": "/NutriTrack/static/js/main.a06c10b4.chunk.js"
+  },
+  {
+    "revision": "c6890abe30fbd066ddeb",
+    "url": "/NutriTrack/static/js/runtime~main.731b0269.js"
+  }
+]);
